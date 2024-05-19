e12eea29653b744f21d3948846487d79dabe0636
Wes Koerber <wes@weskoerber.com>
2024-05-18 21:38:19 -0400

    refactor: cross-platform wrapper
    use switch within function instead of assignment

--- lib/root.zig
+++ lib/root.zig
@@ -2,16 +2,21 @@ pub const MacAddress = @import("MacAddress.zig");
 pub const MacAddressError = @import("errors.zig").MacAddressError;
 
 /// Retrieve all MAC addresses, including loopback devices.
-pub const getAll = switch (native_os) {
-    .linux => linux.getAll,
-    .windows => windows.getAll,
-    else => |x| @panic("Unsupported OS '" ++ @tagName(x) ++ "'"),
-};
+pub fn getAll(allocator: std.mem.Allocator) ![]MacAddress {
+    return switch (native_os) {
+        .linux => linux.getAll(allocator),
+        .windows => windows.getAll(allocator),
+    };
+}
 
-pub const getFirstNoLoopback = switch (native_os) {
-    .linux => linux.getFirstNoLoopback,
-    else => |x| @panic("Unsupported OS '" ++ @tagName(x) ++ "'"),
-};
+/// Retrieve MAC address from the first network interface that is not a
+/// loopback interface.
+pub fn getFirstNoLoopback(allocator: std.mem.Allocator) !MacAddress {
+    return switch (native_os) {
+        .linux => linux.getFirstNoLoopback(allocator),
+        else => |x| @panic("Unsupported OS '" ++ @tagName(x) ++ "'"),
+    };
+}
 
 const std = @import("std");
 const builtin = @import("builtin");
