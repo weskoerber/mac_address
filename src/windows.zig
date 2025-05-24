87b72774cac9d07a98a3eb5e901be4872ad51f9f
Wesley Koerber <wes@weskoerber.com>
2025-05-24 14:55:34 -0400

    chore: exclude devices that don't support ARP (#13)
    * chore: exclude devices that don't support ARP
    
    * refactor: exclude layer 3 devices by default
    
    Linux, Windows:
    - Remove getFirstNoLoopback as we no longer retrieve loopback
      devices
    
    Linux:
    - Explicitly check the interface flags for `IFF_NOARP` and
      `IFF_LOOPBACK`, as `IFF_NOARP` is not set for the loopback device
    
    * chore(linux): use definitions from stdlib
    
    * chore: unconditionally build examples
    
    * chore: refactor print_first example
    
    * chore: rename print first example
    
    * chore: update build.zig.zon
    
    * chore: update mlugg/setup-zig to v2
    
    * chore: update version
    
    * chore: remove linux.getFirstNoLoopback

--- src/windows.zig
+++ src/windows.zig
@@ -21,41 +21,16 @@ pub fn getAll(allocator: mem.Allocator) ![]MacAddress {
 
     var node: ?*IP_ADAPTER_ADDRESSES_LH = &buf[0];
     while (node) |adapter| : (node = node.?.Next) {
+        if (adapter.PhysicalAddressLength == 0) {
+            continue;
+        }
+
         try addrs.append(MacAddress{
             .data = adapter.PhysicalAddress[0..6].*,
         });
     }
 
     return try addrs.toOwnedSlice();
-}
-
-/// Gets the MAC address of the first non-loopback interface.
-///
-/// Internally, this function works identically to the `getAll` function except
-/// that it returns a single `MacAddress` struct, whereas `getAll` returns a
-/// slice of `MacAddress` structs.
-///
-/// The returned memory is stack allocated and returned by value; the caller
-/// need not free anything.
-pub fn getFirstNoLoopback(allocator: mem.Allocator) !MacAddress {
-    const buf = try allocator.alloc(IP_ADAPTER_ADDRESSES_LH, 36);
-    defer allocator.free(buf);
-    var size = @as(u32, @truncate(buf.len * @sizeOf(IP_ADAPTER_ADDRESSES_LH)));
-
-    if (GetAdaptersAddresses(.INET, .{}, null, @alignCast(@ptrCast(buf.ptr)), &size) != @intFromEnum(NO_ERROR)) {
-        return MacAddressError.OsError;
-    }
-
-    var node: ?*IP_ADAPTER_ADDRESSES_LH = &buf[0];
-    while (node) |adapter| : (node = node.?.Next) {
-        if (adapter.IfType != MIB_IF_TYPE_LOOPBACK) {
-            return MacAddress{
-                .data = adapter.PhysicalAddress[0..6].*,
-            };
-        }
-    }
-
-    return MacAddressError.NoDevice;
 }
 
 const std = @import("std");
@@ -66,10 +41,8 @@ const foundation = win32.foundation;
 const ip_helper = win32.network_management.ip_helper;
 
 const NO_ERROR = foundation.NO_ERROR;
-const ERROR_BUFFER_OVERFLOW = foundation.ERROR_BUFFER_OVERFLOW;
 const IP_ADAPTER_ADDRESSES_LH = ip_helper.IP_ADAPTER_ADDRESSES_LH;
 const GetAdaptersAddresses = ip_helper.GetAdaptersAddresses;
 
-const MIB_IF_TYPE_LOOPBACK = ip_helper.MIB_IF_TYPE_LOOPBACK;
 const MacAddress = @import("MacAddress.zig");
 const MacAddressError = @import("errors.zig").MacAddressError;
