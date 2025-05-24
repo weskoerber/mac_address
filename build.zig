11a6b68aba4409d2032c7b90d8ce165b42d023b8
Wes Koerber <wes@weskoerber.com>
2025-05-24 14:25:00 -0400

    chore: unconditionally build examples

--- build.zig
+++ build.zig
@@ -5,28 +5,39 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const examples = b.option(bool, "examples", "Build examples") orelse false;
     const verbose = b.option(bool, "verbose", "Add verbose logging") orelse false;
 
     const mod = b.addModule("mac_address", .{
         .target = target,
         .optimize = optimize,
-        .root_source_file = b.path("lib/root.zig"),
+        .root_source_file = b.path("src/root.zig"),
     });
 
     const build_options = b.addOptions();
     build_options.addOption(bool, "verbose", verbose);
     mod.addOptions("options", build_options);
 
-    if (examples) {
-        buildExamples(
-            b,
-            .{ .name = "mac_address", .module = mod },
-            .{ .target = target, .optimize = optimize },
-        ) catch |err| {
-            std.debug.print("error: {}\n  -> failed building examples\n", .{err});
-        };
-    }
+    const exe1 = b.addExecutable(.{
+        .name = "print_all",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .root_source_file = b.path("examples/print_all.zig"),
+        }),
+    });
+    exe1.root_module.addImport("mac_address", mod);
+    b.installArtifact(exe1);
+
+    const exe2 = b.addExecutable(.{
+        .name = "print_first_no_loopback",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .root_source_file = b.path("examples/print_first_no_loopback.zig"),
+        }),
+    });
+    exe2.root_module.addImport("mac_address", mod);
+    b.installArtifact(exe2);
 
     addDocsStep(b, .{ .target = target, .optimize = optimize });
 
@@ -35,7 +46,7 @@ pub fn build(b: *std.Build) void {
     const test_exe = b.addTest(.{
         .target = target,
         .optimize = optimize,
-        .root_source_file = b.path("lib/root.zig"),
+        .root_source_file = b.path("src/root.zig"),
     });
     test_exe.root_module.addOptions("options", build_options);
 
@@ -44,9 +55,9 @@ pub fn build(b: *std.Build) void {
 
     if (target.result.os.tag == .windows) {
         const zigwin32_mod = b.dependency("zigwin32", .{
-            .target = target,
-            .optimize = optimize,
-        }).module("zigwin32");
+            // .target = target,
+            // .optimize = optimize,
+        }).module("win32");
 
         mod.addImport("win32", zigwin32_mod);
         mod.pic = true;
@@ -55,39 +66,12 @@ pub fn build(b: *std.Build) void {
     }
 }
 
-fn buildExamples(b: *std.Build, import: std.Build.Module.Import, options: anytype) !void {
-    const examples_dir = try b.build_root.join(b.allocator, &.{"examples"});
-    const dir = try b.build_root.handle.openDir(examples_dir, .{ .iterate = true });
-    var iter = dir.iterate();
-
-    while (try iter.next()) |example_src| {
-        const extension_pos = mem.indexOfScalar(u8, example_src.name, '.').?;
-
-        const root_source_file = b.pathJoin(&.{
-            "./examples",
-            example_src.name,
-        });
-
-        const exe_name = example_src.name[0..extension_pos];
-
-        const exe = b.addExecutable(.{
-            .name = exe_name,
-            .target = options.target,
-            .optimize = options.optimize,
-            .root_source_file = b.path(root_source_file),
-        });
-
-        exe.root_module.addImport(import.name, import.module);
-        b.installArtifact(exe);
-    }
-}
-
 fn addDocsStep(b: *std.Build, options: anytype) void {
     const docs_step = b.step("docs", "Emit docs");
 
     const lib = b.addStaticLibrary(.{
         .name = "mac_address",
-        .root_source_file = b.path("lib/root.zig"),
+        .root_source_file = b.path("src/root.zig"),
         .target = options.target,
         .optimize = options.optimize,
     });
