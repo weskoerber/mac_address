cfd10cf4abcf2f631c53d4c0194840554a714091
Wes Koerber <wes@weskoerber.com>
2025-05-24 14:27:00 -0400

    chore: refactor print_first example

--- examples/print_first_no_loopback.zig
+++ examples/print_first_no_loopback.zig
@@ -5,10 +5,14 @@ const heap = std.heap;
 const mac_address = @import("mac_address");
 
 pub fn main() void {
-    const addr = mac_address.getFirstNoLoopback(heap.page_allocator) catch |err| {
+    const addrs = mac_address.getAll(heap.page_allocator) catch |err| {
         debug.print("error: {}\n  -> unable to get MAC address\n", .{err});
         return;
     };
 
-    debug.print("{}\n", .{addr});
+    if (addrs.len == 0) {
+        std.debug.print("No MAC addresses found\n", .{});
+    } else {
+        std.debug.print("{}\n", .{addrs[0]});
+    }
 }
